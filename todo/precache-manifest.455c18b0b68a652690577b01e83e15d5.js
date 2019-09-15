79492d36d216887f635918107cd99f328b4526f4
L1nks <l1nks@inbox.ru>
2019-09-15 21:08:42 +0500

    todo list

---
+++ todo/precache-manifest.455c18b0b68a652690577b01e83e15d5.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4e048fc797359bd39f190b9b2cebe1a0",
+    "url": "/index.html"
+  },
+  {
+    "revision": "b65ce069b492bb0554db",
+    "url": "/static/css/main.c82cfe6c.chunk.css"
+  },
+  {
+    "revision": "06f44d7c6eeee966ea35",
+    "url": "/static/js/2.34ad89f7.chunk.js"
+  },
+  {
+    "revision": "b65ce069b492bb0554db",
+    "url": "/static/js/main.82516771.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
