351ca757cc3c5dd37461e21122644cce41c865bf
L1nks <l1nks@inbox.ru>
2019-09-22 19:52:26 +0500

    jokes react build

---
+++ Jokes/precache-manifest.57b8fef87a5af68e1bdfc4a83d7c4313.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1617f1aa408484f75406065e4fcd2489",
+    "url": "/index.html"
+  },
+  {
+    "revision": "c0048c2bfabc667963f4",
+    "url": "/static/css/main.9ca35b8d.chunk.css"
+  },
+  {
+    "revision": "85121adc0fedbc4f8bf5",
+    "url": "/static/js/2.356ff5d8.chunk.js"
+  },
+  {
+    "revision": "c0048c2bfabc667963f4",
+    "url": "/static/js/main.47cd487d.chunk.js"
+  },
+  {
+    "revision": "007a4baee4f34915d9f3",
+    "url": "/static/js/runtime~main.95bdf8eb.js"
+  }
+]);
