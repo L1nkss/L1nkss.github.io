1eee29dfe4469919ce936a5abd5fd0c529e7f709
L1nks <l1nks@inbox.ru>
2019-10-02 23:07:50 +0500

    update modulbank

--- modulbank/script/index.js
+++ modulbank/script/index.js
@@ -1,7 +1,10 @@
-(function () {
-  document.addEventListener("DOMContentLoaded", function() {
-    //The first argument are the elements to which the plugin shall be initialized
-    //The second argument has to be at least a empty object or a object with your desired options
-    OverlayScrollbars(document.querySelectorAll('.account'), { });
-  });
+(function() {
+    const toggleMenuButton = document.querySelector('.burger');
+    const menu = document.querySelector('.navigation');
+    const burgerToggle = document.querySelector('.burger__toggle');
+
+    toggleMenuButton.addEventListener('click', () => {
+        menu.classList.toggle('navigation--toggle');
+        burgerToggle.classList.toggle('burger__toggle--hide')
+    });
 })();
